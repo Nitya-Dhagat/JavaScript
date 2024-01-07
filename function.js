22d3b644513976221c8403d0a5cb00af2c349195
Nitya Dhagat <nityadhagatddit@gmail.com>
2024-01-07 14:39:44 +0530

    changing variable declaration

--- function.js
+++ function.js
@@ -1,6 +1,8 @@
-var username = "Nitya Dhagat";
-var age = 19;
-var hasHobbies = "Coding";
+const username = "Nitya Dhagat";    //const keyword doesn't allow the variable to be changed further in the code
+let age = 15;                       //let is as same as using var or new keyword for declaring variables
+let hasHobbies = "Coding";
+
+age = 19;
 
 function summarizeUser(userName,userAge,userHobbies){
     return (
